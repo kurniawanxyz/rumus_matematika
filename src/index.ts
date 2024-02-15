850a0149a637cbb7bdd8d89110abf1f8e6e3134d
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-16 02:51:06 +0700

    feat()=> kurang,bagi ;

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,12 @@ const tambah=(...angka:number[]) => {
     const result = angka.reduce((prevdata,currentData)=>prevdata+currentData)
     return result
 };
+
+const kurang=(...angka:number[])=>{
+    const result = angka.reduce((prevdata,currentData)=>prevdata-currentData)
+    return result
+}
+
+const bagi=(angka1:number,angka2:number)=>{
+    return angka1/angka2
+}
