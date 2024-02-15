08cc877c2f7bc6255432130f71e3e5b1bd4f5bf2
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-16 02:47:47 +0700

    feat()=> tambah

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@
 const mean = (jumlahData:number,banyakData:number):number =>{
     return jumlahData/banyakData
 }
+
+const tambah=(...angka:number[]) => {
+    const result = angka.reduce((prevdata,currentData)=>prevdata+currentData)
+    return result
+};
