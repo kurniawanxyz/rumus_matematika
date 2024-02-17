058a27640cf41a2e88ed5d25484dc7e605e9c426
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-17 22:24:29 +0700

    publish 1.1.11

--- src/index.ts
+++ src/index.ts
@@ -41,21 +41,21 @@ export class Fisika {
   /**
    * Menghitung kecepatan dengan rumus jarak dibagi waktu
    */
-  kecepatan(jarak: number, waktu: number): number {
+  static kecepatan(jarak: number, waktu: number): number {
     return jarak / waktu;
   }
 
   /**
    * Menghitung jarak dengan rumus kecepatan dikali waktu
    */
-  jarak(kecepatan: number, waktu: number): number {
+  static jarak(kecepatan: number, waktu: number): number {
     return kecepatan * waktu;
   }
 
   /**
    * Menghitung waktu dengan rumus jarak dibagi kecepatan
    */
-  waktuKecepatan(jarak: number, kecepatan: number): number {
+  static waktuKecepatan(jarak: number, kecepatan: number): number {
     return jarak / kecepatan;
   }
 }
@@ -65,21 +65,21 @@ export class VolumeDebit {
   /**
    * Menghitung debit dengan rumus volume dibagi waktu
    */
-  debit(volume: number, waktu: number): number {
+  static debit(volume: number, waktu: number): number {
     return volume / waktu;
   }
 
   /**
    * Menghitung volume dengan rumus debit dikali waktu
    */
-  volume(debit: number, waktu: number): number {
+  static volume(debit: number, waktu: number): number {
     return debit * waktu;
   }
 
   /**
    * Menghitung waktu dengan rumus volume dibagi debit
    */
-  waktuDebit(debit: number, kecepatan: number): number {
+  static waktuDebit(debit: number, kecepatan: number): number {
     return debit / kecepatan;
   }
 }
@@ -89,21 +89,21 @@ export class PetaSkala {
   /**
    * Menghitung skala peta
    */
-  skala(jarakPadaPeta: number, jarakSebenarnya: number): string {
+  static skala(jarakPadaPeta: number, jarakSebenarnya: number): string {
     return `${jarakPadaPeta} : ${jarakSebenarnya}`;
   }
 
   /**
    * Menghitung jarak sebenarnya dari skala peta
    */
-  jarakSebenarnya(jarakPadaPeta: number, jarakSebenarnya: number): number {
+  static jarakSebenarnya(jarakPadaPeta: number, jarakSebenarnya: number): number {
     return jarakPadaPeta / (jarakPadaPeta / jarakSebenarnya);
   }
 
   /**
    * Menghitung jarak pada peta dari jarak sebenarnya
    */
-  jarakPadaPeta(jarakPadaPeta: number, jarakSebenarnya: number): number {
+  static jarakPadaPeta(jarakPadaPeta: number, jarakSebenarnya: number): number {
     return (jarakPadaPeta / jarakSebenarnya) * jarakSebenarnya;
   }
 }
@@ -113,42 +113,42 @@ export class Trigonometri {
   /**
    * Menghitung nilai sinus dari sudut dalam derajat
    */
-  sin(derajat: number): number {
+  static sin(derajat: number): number {
     return Math.sin(derajat * (Math.PI / 180));
   }
 
   /**
    * Menghitung nilai cosinus dari sudut dalam derajat
    */
-  cos(derajat: number): number {
+  static cos(derajat: number): number {
     return Math.cos(derajat * (Math.PI / 180));
   }
 
   /**
    * Menghitung nilai tangen dari sudut dalam derajat
    */
-  tan(derajat: number): number {
+  static tan(derajat: number): number {
     return Math.tan(derajat * (Math.PI / 180));
   }
 
   /**
    * Menghitung nilai arcsin dari sebuah nilai
    */
-  asin(nilai: number): number {
+  static asin(nilai: number): number {
     return Math.asin(nilai) * (180 / Math.PI);
   }
 
   /**
    * Menghitung nilai arccos dari sebuah nilai
    */
-  acos(nilai: number): number {
+  static acos(nilai: number): number {
     return Math.acos(nilai) * (180 / Math.PI);
   }
 
   /**
    * Menghitung nilai arctan dari sebuah nilai
    */
-  atan(nilai: number): number {
+  static atan(nilai: number): number {
     return Math.atan(nilai) * (180 / Math.PI);
   }
 }
@@ -158,7 +158,7 @@ export class Statistik {
   /**
    * Menghitung rata-rata dari sebuah array angka
    */
-  rataRataArray(arr: number[]): number {
+  static rataRataArray(arr: number[]): number {
     return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
   }
 }
